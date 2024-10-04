b71fa6539465f170d7eb251f29a3662902e6c852
PraveenT25 <praveentavari25@gmail.com>
2024-10-04 22:38:31 +0530

    add task working

---
+++ app.js
@@ -0,0 +1,23 @@
+let listcontainer = document.getElementById('list-container')
+let inputBox = document.getElementById('input-box')
+
+
+function add_task(){
+    if(inputBox.value === ''){
+        alert('Please enter a task')
+    }
+
+    // Add the task
+    else{
+        let task = document.createElement('li')
+        task.textContent = inputBox.value
+        listcontainer.appendChild(task)
+        
+        let span = document.createElement('span');
+        span.textContent = '\u00d7';
+        task.appendChild(span)
+    }
+
+    //after adding the task input box will be emplty 
+    inputBox.value = ''
+}
