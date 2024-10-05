821a37836425dae329fc4b0fa1efb8f3fb206970
PraveenT25 <praveentavari25@gmail.com>
2024-10-05 22:49:29 +0530

    finaly we can add and delete the tasks

--- app.js
+++ app.js
@@ -20,5 +20,32 @@ function add_task(){
 
     //after adding the task input box will be emplty 
     inputBox.value = ''
-    
+    saveData()
+  
 }
+
+//deleting the task
+
+listcontainer.addEventListener('click', (el)=>{
+    if(el.target.tagName == 'LI'){
+        el.target.classList.toggle('checked')
+        saveData()
+    }
+    else if(el.target.tagName == 'SPAN'){
+        el.target.parentElement.remove()
+        saveData()
+    }
+})
+
+//local storage use for when we refresh the page task will not removed
+function saveData(){
+    localStorage.setItem("tasks", listcontainer.innerHTML)
+} 
+
+function showData()
+{
+    listcontainer.innerHTML = localStorage.getItem('tasks')
+
+}
+
+showData()
